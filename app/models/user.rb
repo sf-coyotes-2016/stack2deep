5f9131f670c0c2d2f6063b9dc6fd53b89c1def4f
“JulianBoralli” <jboralli@icloud.com>
2016-11-03 15:45:14 -0700

    Create migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,7 @@
-require 'bcrypt'
-
 class User < ActiveRecord::Base
   # users.password_hash in the database is a :string
   include BCrypt
 
-	validates_presence_of :first_name
-  validates_presence_of :last_name
-  validates_presence_of :username
-  validates_uniqueness_of :username
   validates_presence_of :password
   validates_presence_of :email
   validates_uniqueness_of :email
